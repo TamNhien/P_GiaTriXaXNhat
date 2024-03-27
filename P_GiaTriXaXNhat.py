c0ad68bba1862bb069fcbf4d97bb0bb184a73f65
TamNhien <canhthien91@gmail.com>
2024-03-27 20:14:27 +0700

    Add project files.

---
+++ P_GiaTriXaXNhat.py
@@ -0,0 +1,6 @@
+﻿def find_value_farthest_from_x(lst, x):
+    return max(lst, key=lambda num: abs(num - x))
+
+lst = list(map(int, input("Nhập vào danh sách các số nguyên, cách nhau bởi dấu phẩy : ").split(',')))
+x = int(input("Nhập vào số nguyên x: "))
+print("Giá trị trong danh sách xa", x, "nhất là : ", find_value_farthest_from_x(lst, x))
